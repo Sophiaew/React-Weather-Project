6d3755f8df67434aff59dfaa76d4f8f3558fd697
Sophiaew <Sophiaew@live.com>
2022-02-07 10:51:16 -0500

    eer

--- src/App.js
+++ src/App.js
@@ -2,5 +2,12 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <a href="https://github.com/Sophiaew/React-Weather-Project">
+        Open Source
+      </a>
+    </div>
+  );
 }
