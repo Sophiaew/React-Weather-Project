e7b1ba0a58d9619ebb3feb3d6d14057cbe6f8cbc
Sophiaew <Sophiaew@live.com>
2022-02-08 17:12:55 -0500

    conversion

--- src/Weather.js
+++ src/Weather.js
@@ -32,8 +32,7 @@ export default function Weather(props) {
 
   function search() {
     const apiKey = "17bcfd67e085423bef87d025a6a15b1b";
-    let unit = "metric";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${unit}`;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(handleResponse);
   }
 
